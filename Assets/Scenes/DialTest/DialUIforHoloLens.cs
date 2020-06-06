89cd3d21d97e9fe6575b85eb7b609a83c982a6e7
Sthesia <seirios48@yahoo.co.jp>
2020-06-06 15:08:11 +0900

    Implementing the event Completing the SwitchUI prototype

--- Assets/Scenes/DialTest/DialUIforHoloLens.cs
+++ Assets/Scenes/DialTest/DialUIforHoloLens.cs
@@ -1,19 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Microsoft.MixedReality.Toolkit.UI;
 
+[RequireComponent(typeof(Interactable))]
 public class DialUIforHoloLens: MonoBehaviour
 {
+    /// <summary>
+    /// The event that occurs when the target is moved to the right
+    /// </summary>
+    public UnityEvent OnUpdateRightAxisEvent;
+
+    public UnityEvent OnUpdateLeftAxisEvent;
+
+    public UnityEvent OnUpdateUpAxisEvent;
+
+    public UnityEvent OnUpdateDownAxisEvent;
     //target interactable.cs
     Interactable inter;
 
+    /// <summary>
+    /// Radius to recognize the input
+    /// </summary>
+    [SerializeField, TooltipAttribute("Radius to recognize the input")]
+    [Range(0,1)]
+    public float MinRange = 0.3f;
+
+    public float MaxRange = 1.0f;
     //Action On/Off
     bool _ActionEnebled;
-    //transform
-    Vector3 dpos;
-
-    public Theme InteractableStates;
+    bool _OnUpdaterightAxisEvent =true;
+    bool _OnUpdateleftAxisEvent = true;
+    bool _OnUpdateupperAxisEvent = true;
+    bool _OnUpdatedownAxisEvent = true;
+   //transform
+   Vector3 dpos;
+    
 
     private void Start()
     {
@@ -27,48 +50,105 @@ public class DialUIforHoloLens: MonoBehaviour
             dpos = this.gameObject.transform.localPosition;
             // x axis input is pluse value 
 
-            
+            float yaxisparm = System.Math.Abs(dpos.y);
+            float xaxisparm = System.Math.Abs(dpos.x);
+
             bool result = System.Math.Abs(dpos.y) > System.Math.Abs(dpos.x);
-            bool resulty = dpos.y>0;
-            bool resultx = dpos.x> 0;
+            bool resulty = dpos.y>MinRange;
+            bool resultx = dpos.x> MinRange;
+   
+               if(!resultx && !resulty)
+               {
+                  return;
+               }
+
                if(result && resulty)
                {
-                     Result_Yp();
-               }
+                     Result_Yp(yaxisparm);
+                if (_OnUpdateupperAxisEvent)
+                {
+                    reslutUpperAxisEvent();
+                    _OnUpdateupperAxisEvent = false;
+                }
+            }
+
                if (result && !resulty)
                {
-                    Result_Ym();
-               }
+                    Result_Ym(yaxisparm);
+                if (_OnUpdatedownAxisEvent)
+                {
+                    reslutDownAxisEvent();
+                    _OnUpdatedownAxisEvent = false;
+                }
+            }
+
                if (!result && resultx)
                {
-                    Result_Xp();
+                    Result_Xp(xaxisparm);
+                if (_OnUpdaterightAxisEvent) 
+                {
+                    reslutRightAxisEvent();
+                    _OnUpdaterightAxisEvent = false;
+                }
                }
+
                if(!result && !resultx )
                {
-                    Result_Xm();
-               }
-            
-
-
+                    Result_Xm(xaxisparm);
+                if (_OnUpdateleftAxisEvent)
+                {
+                    resultLeftAxisEvent();
+                    _OnUpdateleftAxisEvent = false;
+                }
+               }          
         }
 
     }
 
-    void Result_Yp()
+    void Result_Yp(float yaxis)
     {
-        Debug.Log("Y Axis");
+        //Debug.Log("Y Axis");  
     }
-    void Result_Ym()
+    void Result_Ym(float yaxis)
     {
-        Debug.Log("-Y Axis");
+        //Debug.Log("-Y Axis");
     }
-    void Result_Xp()
+    void Result_Xp(float xaxis)
     {
-        Debug.Log("X Axis");
+        //Debug.Log("X Axis");
     }
-    void Result_Xm()
+    void Result_Xm(float xaxis)
     {
-        Debug.Log("-X Axis");
+       // Debug.Log("-X Axis");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void reslutRightAxisEvent()
+    {
+        Debug.Log("Right");
+        OnUpdateRightAxisEvent.Invoke();
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    void resultLeftAxisEvent()
+    {
+        Debug.Log("Left");
+        OnUpdateLeftAxisEvent.Invoke();
+    }
+
+    void reslutUpperAxisEvent()
+    {
+        Debug.Log("Up");
+        OnUpdateUpAxisEvent.Invoke();
+    }
+
+    void reslutDownAxisEvent()
+    {
+        Debug.Log("Down");
+        OnUpdateDownAxisEvent.Invoke();
     }
 
     /// <summary>
@@ -86,6 +166,9 @@ public class DialUIforHoloLens: MonoBehaviour
     {
         Debug.Log("pointerAnimationEnd");
         _ActionEnebled = false;
+        //Reset trigger
+        _OnUpdaterightAxisEvent = true;
+        _OnUpdateleftAxisEvent = true;
     }
     /// <summary>
     /// This method call by ManipulationHundeler OnManiputationEnded.
