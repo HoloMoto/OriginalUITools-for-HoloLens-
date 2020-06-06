9409038b0dce07a6a241eb19a8a6d8acd776f8d2
Sthesia <seirios48@yahoo.co.jp>
2020-06-06 12:20:38 +0900

    Creating a script that detects the position of an object and provides a reset function

---
+++ Assets/Scenes/DialTest/DialUIforHoloLens.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class DialUIforHoloLens: MonoBehaviour
+{
+    //target interactable.cs
+    public Interactable inter;
+
+    //Action On/Off
+    public bool _ActionEnebled;
+
+    //transform
+    Vector3 dpos;
+    private void Start()
+    {
+        inter = this.gameObject.GetComponent<Interactable>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_ActionEnebled)
+        {
+            dpos = this.gameObject.transform.localPosition;
+            // x axis input is pluse value 
+
+            
+            bool resultyx = dpos.normalized.y > dpos.normalized.x;
+            if (resultyx && dpos.y>0)
+            {
+                  Result_Yp();
+            }
+            if (resultyx && 0 > dpos.y)
+            {
+                Result_Xm();
+            }
+            if (!resultyx && dpos.x>0)
+            {
+                Result_Xp();
+            }
+            if(!resultyx && 0>dpos.x)
+            {
+                Result_Ym();
+            }
+            resultyx = dpos.normalized.y > dpos.normalized.x;
+
+
+        }
+  
+    }
+
+    void Result_Yp()
+    {
+        Debug.Log("Y Axis");
+    }
+    void Result_Ym()
+    {
+        Debug.Log("-Y Axis");
+    }
+    void Result_Xp()
+    {
+        Debug.Log("X Axis");
+    }
+    void Result_Xm()
+    {
+        Debug.Log("-X Axis");
+    }
+
+    /// <summary>
+    /// 移動中に呼びたいヤツ
+    /// </summary>
+    public void PointerAnimationStat()
+    {
+        Debug.Log("pointerAnimationStart");
+        _ActionEnebled = true;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public void PointerAnimationEnd()
+    {
+        Debug.Log("pointerAnimationEnd");
+        _ActionEnebled = false;
+    }
+    /// <summary>
+    /// This method call by ManipulationHundeler OnManiputationEnded.
+    /// </summary>
+    public void PointerReset()
+    {
+        //Pointer Transform Reset
+        Debug.Log("PointerReset");
+        this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+        this.gameObject.transform.localRotation = new Quaternion(1, 0, 0, 1);
+    }
+}
